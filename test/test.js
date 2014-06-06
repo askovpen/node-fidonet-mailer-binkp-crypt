77a5f67cb8982922712416cf22597502491c9cbb
Alexander Skovpen <a.n.skovpen@gmail.com>
2014-06-07 00:26:42 +0400

    test changed

--- test/test.js
+++ test/test.js
@@ -5,13 +5,11 @@ describe('Fidonet BinkP crypt module', function(){
 	var client=crypt('test');
 	it('calculates correct encoding', function(){
 		client.init_keys();
-		buf=new Buffer([0x80,0x01,0x05]);
-		assert.deepEqual(client.encrypt_buf(buf),new Buffer([0x5f,0x9f,0x26]));
+		assert.deepEqual(client.encrypt_buf(new Buffer([0x80,0x01,0x05])),new Buffer([0x5f,0x9f,0x26]));
 	});
 	it('calculates correct decoding', function(){
 		client.init_keys();
-		var buf=new Buffer([0x5f,0x9f,0x26]);
-		assert.deepEqual(client.decrypt_buf(buf),new Buffer([0x80,0x01,0x05]));
+		assert.deepEqual(client.decrypt_buf(new Buffer([0x5f,0x9f,0x26])),new Buffer([0x80,0x01,0x05]));
 	});
 	client.init_keys();
 });
